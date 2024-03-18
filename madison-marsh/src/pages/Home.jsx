f781317f047cbc15a135ec31b2b935e17c741385
hftrujillo <hftrujillo@loyola.edu>
2024-03-18 12:43:23 -0400

    Test Flowbite library

--- madison-marsh/src/pages/Home.jsx
+++ madison-marsh/src/pages/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      Home
+      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Test</button>
+    </div>
   )
 }
 
