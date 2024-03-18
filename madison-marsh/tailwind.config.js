4be1a3b9cfaaa84ce268191f0887b0fef14ea40c
hftrujillo <hftrujillo@loyola.edu>
2024-03-18 12:23:45 -0400

    Added Color Pallette

--- madison-marsh/tailwind.config.js
+++ madison-marsh/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'MMHCBrown' : '#362706',
+        'MMHCGreen' : '#464E2E',
+        'MMHCOlive' : '#ACB992',
+        'MMHCCream' : '#E9E5D6'
+      }
+    },
   },
   plugins: [],
 }
