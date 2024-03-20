4248083b5944d713102559a2fe67a841ce3a8fd2
hftrujillo <hftrujillo@loyola.edu>
2024-03-19 22:36:24 -0400

    VD5: Added pictures on cards, messed with dimensions but they look nice

--- madison-marsh/src/sections/ThreeCards.jsx
+++ madison-marsh/src/sections/ThreeCards.jsx
@@ -1,23 +1,26 @@
 import { Card } from '../components';
+import sika from '../assets/images/sika.jpg';
+import group from '../assets/images/hunting-group.jpg';
+import dorcester from '../assets/images/dorcester.jpg';
 
 const ThreeCards = () => {
   return (
     <threecards>
         <div className = "bg-MMHCOlive p-5 flex flex-row place-content-around">
             <Card
-                imageUrl="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                imageUrl={group}
                 altText="Club info"
                 title="Club Information"
                 description="Click here to access club information"
             />
             <Card
-                imageUrl="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                imageUrl={sika}
                 altText="Hunting Resources"
                 title="Hunting Resources"
                 description="Click here to access external hunting links"
             />
             <Card
-                imageUrl="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                imageUrl={dorcester}
                 altText="History"
                 title="History"
                 description="Click here to read about the rich history of Dorcester County"
