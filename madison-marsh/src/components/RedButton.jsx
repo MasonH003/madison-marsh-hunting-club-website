217c5e4f273a6559381e872d27c60382e9531607
Mason Hall <mlhall1@loyola.edu>
2024-04-24 12:00:25 -0400

    GM2 Moving ui of account details

--- madison-marsh/src/components/RedButton.jsx
+++ madison-marsh/src/components/RedButton.jsx
@@ -1,6 +1,6 @@
 const RedButton = ({label, onClick}) => {
     return (
-        <button onClick = {onClick} class="btn btn-outline flex flex-row border-2 text-MMHCRed border-MMHCRed bg-MMHCCream hover:text-MMHCCream hover:bg-MMHCLightRed">
+        <button onClick = {onClick} class="btn btn-error btn-outline flex flex-row border-2 text-MMHCRed border-MMHCRed bg-MMHCCream hover:text-MMHCCream hover:bg-MMHCLightRed">
             {label}
         </button>
     )
--- madison-marsh/src/pages/AccountOverview.jsx
+++ madison-marsh/src/pages/AccountOverview.jsx
@@ -5,18 +5,14 @@ import { useNavigate } from 'react-router-dom';
 import {RedButton} from '../components';
 import React, { useEffect, useState } from 'react';
 
+
 function AccountOverview() {
   
     const navigate = useNavigate();
 
-    const fetchUserData = async () => {
-
-        try {
-            const { data: { user } } = await supabase.auth.getUser();
-        } catch (error) {
-            console.error("Error fetching user data:", error.message);
-        }
-    }
+    const [currUser, setCurrUser] = useState(null);
+    const [userEmail, setUserEmail] = useState("");
+    const [userId, setUserId] = useState(-1);
 
     const signOutUser = async () => {
         try {
@@ -45,38 +41,71 @@ function AccountOverview() {
       // Incomplete
       const updateUsername = async({newUsername}) => {
         const { error } = await supabase
-            .from('users')
-            .update({ display_name : {newUsername} })
-            .eq('id', 1)
+            .from('userdata')
+            .update({ username : {newUsername} })
+            .eq('id', user.id)
       }
 
       const [visible, setVisible ] = useState(false);
-  
+
+      const fetchUserData = async () => {
+        try {
+            const { data: { user } } = await supabase.auth.getUser();
+            setCurrUser( user );
+            setUserEmail( user.email );
+            //setUserId( user.id );
+            console.log("email is " + user.email );
+        } catch (error) {
+            console.error("Error fetching user data:", error.message);
+            navigate('/SigninSignup')
+        }
+        /*try {
+          const { userdata, error } = await supabase
+              .from('userdata')
+              .select('username, permission')
+              .eq('id', user.id)
+              .single()
+        } catch (error) {
+          
+          const { error2 } = await supabase
+          .from('userdata')
+          .insert({ id: user.id, username: "user", permission: 0 })
+        }*/
+    }
+      
+    useEffect(() => {  //using "useEffect" stops the component from continuously calling this method and rerendering forever
+      fetchUserData();
+  }, []);
+
     return (
-    <accoverview>
-      <Header/>
-      <div className = "text-4xl font-thin text-MMHCCream bg-MMHCBlack flex flex-row justify-center p-4">
-        Account Overview
-      </div>
-      <div className = "bg-MMHCBlack text-MMHCCream flex flex-col items-center">
+      <accoverview>
+        <Header/>
+        <div className = "text-4xl font-thin text-MMHCCream bg-MMHCBlack flex flex-row justify-center p-4">
+          Account Overview 
+          
+        </div>
+        <div className = "text-4xl justify-left flex pb-4">
+        <p>Email: {userEmail}</p>
+        </div>
+        <div className = "bg-MMHCBlack text-MMHCCream flex flex-col items-center">
 
-        <RedButton onClick = {signOutUser} label = {'Logout'}/>
-        <RedButton onClick = {()=> setVisible(!visible)} label = {'Change Username'}/>
-        {visible ? 
-            <div className={"visible"}>
-              <form action={updateUsername} class="p-2"> New Username:
-                <input name="query" class="p-2 rounded text-MMHCBlack" />
-                <button type="submit" class="bg-MMHCRed border-MMHCBlack rounded p-2">Submit</button>
-              </form>
-            </div>
-            :
-            <div className={"invisible"}></div>
-          }
-      </div>
+          <RedButton onClick = {signOutUser} label = {'Logout'}/>
+          <RedButton onClick = {()=> setVisible(!visible)} label = {'Change Username'}/>
+          {visible ? 
+              <div className={"visible"}>
+                <form action={updateUsername} class="p-2"> New Username:
+                  <input name="query" class="p-2 rounded text-MMHCBlack" />
+                  <button type="submit" class="bg-MMHCRed border-MMHCBlack rounded p-2">Submit</button>
+                </form>
+              </div>
+              :
+              <div className={"invisible"}></div>
+            }
+        </div>
 
 
-      <Footer/>
-    </accoverview>
+        <Footer/>
+      </accoverview>
   );
 }
 
