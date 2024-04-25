9081b24c5601fe68fc3057c81cdee37dcd6157d5
hftrujillo <hftrujillo@loyola.edu>
2024-04-25 06:51:06 -0400

    Create MemberPage placeholders

--- madison-marsh/src/pages/MemberPage.jsx
+++ madison-marsh/src/pages/MemberPage.jsx
@@ -1,10 +1,29 @@
 import { Header, Footer } from '../sections';
+import { Card } from '../components';
+import birds from '../assets/images/birds.jpg';
+import center from '../assets/images/center.jpg';
 
 const MemberPage = () => {
+
   return (
-    <div>
+    <div className = "">
         <Header/>
-        <h1>This is Members page</h1>
+            <div className = "flex flex-row space-x-6 justify-center pt-6 pb-6 bg-accent">
+                <Card
+                    imageUrl={birds}
+                    altText="Photo Gallery"
+                    title="Photo Gallery"
+                    description="Click here to view members only pictures"
+                    href = "/ClubRules"
+                />
+                <Card
+                    imageUrl={center}
+                    altText="Private Forum"
+                    title="Private Forum"
+                    description="Click here to communicate with other club members"
+                    href = "/ClubRules"
+                />
+            </div>
         <Footer/>
     </div>
   )
